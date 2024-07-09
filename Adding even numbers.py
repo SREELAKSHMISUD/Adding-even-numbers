63429abb5897c8d03abadd7e1df5071c6e3cf20a
SREELAKSHMI SUDHEER 1111 <121602892+SREELAKSHMISUD@users.noreply.github.com>
2024-07-09 21:39:33 +0530

    Add files via upload

---
+++ Adding even numbers.py
@@ -0,0 +1,7 @@
+#adding even numbers
+target = int(input("Enter a number between 0 and 1000:")) # Enter a number between 0 and 1000
+
+total=0
+for number in range(0,target+1,2):
+  total+=number
+print(total)
